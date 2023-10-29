2e6454707c04a80756df9c1d81941fb5dddc2a9b
Alexander <oleksandr.kulachinsky@lengin.com>
2023-10-29 10:39:47 +0100

    update

--- src/templates/layout/L-Filter-block.vue
+++ src/templates/layout/L-Filter-block.vue
@@ -2,41 +2,52 @@
   <div>
     <div class="l-filter">
       <div class="widget">
-        <Select-filter-form @select-filter="sortHendler" ref="testFilter" :options="filterBrand" filter-param="brand" current-option="Бренд" />
+        <Select-filter-form 
+          @select-filter="sortHendler" 
+          ref="select" 
+          :options="filterBrand" 
+          filter-param="brand" 
+          :current-option="brandSelect" 
+        />
       </div>
       <div class="widget">
-        <Select-filter-form @select-filter="sortHendler" :options="filterSize" filter-param="size" current-option="Размер" />
+        <Select-filter-form 
+          @select-filter="sortHendler" 
+          :options="filterSize" 
+          filter-param="size" 
+          :current-option="sizeSelect" 
+        />
       </div>
     </div>
     <div class="l-shop__result">
       <div class="l-shop__result-count">
         <p class="woocommerce-result-count">Отображение 1–16 из 750</p>
       </div>
-      <Select-filter-form @select-filter="sortHendler" :options="filterOrderby" filter-param="orderby" current-option="По новизне" />
+      <Select-filter-form 
+        @select-filter="sortHendler" 
+        :options="filterOrderby" 
+        filter-param="orderby" 
+        :current-option="orderbySelect" 
+      />
     </div>
   </div>
 </template>
 
 <script>
 import SelectFilterForm from '@/templates/forms/SelectFilterForm.vue'
-// import ProductFiltersData from '@/config/productFilterNew'
+import ProductFiltersData from '@/config/productFilterNew'
 import { mapActions } from 'vuex'
 
-// const sortTypes = {
-//   priceAsc: 'price-asc',
-//   priceDesc: 'price-desc'
-// }
-
 export default {
-  // data() {
-  //   return {
-  //     currentFilters: [],
-  //     filterOrderby: ProductFiltersData.orderby,
-  //     filterBrand: ProductFiltersData.brand,
-  //     filterSize: ProductFiltersData.size, 
-  //     filterTypes: ProductFiltersData.types, 
-  //   }
-  // },
+  data() {
+    return {
+      currentFilters: [],
+      filterOrderby: ProductFiltersData.orderby,
+      filterBrand: ProductFiltersData.brand,
+      filterSize: ProductFiltersData.size, 
+      filterTypes: ProductFiltersData.types, 
+    }
+  },
 
   components: {
     SelectFilterForm,
@@ -44,70 +55,19 @@ export default {
 
   computed: {
     categorySlugFromRoute() { return this.$route.params.subcategorySlug || this.$route.params.categorySlug },
-    // currentPage() { 
-    //   const queryPage = this.$route?.query?.page;
-    //   const currentPageNumber = Number(queryPage) || 1;
+    brandSelect() { return this.$route.query.brand || 'Бренд'},
+    sizeSelect() { return this.$route.query.size || 'Размер' },
+    orderbySelect() {return this.$route.query.orderby || 'По новизне'}
+  },
 
-    //   return currentPageNumber;
-    // }
+  mounted() {
+    console.log(this.$route);
   },
 
   methods: {
     ...mapActions({
       fetchProducts: 'catalog/fetchProducts'
     }),
-
-    // ...mapMutations({
-    //   sortProductsByUpPrice: 'catalog/sortProductsByUpPrice',
-    //   sortProductsByDownPrice: 'catalog/sortProductsByDownPrice',
-    //   filterProductsByBrand: 'catalog/filterProductsByBrand',
-    //   filterProductsBySize: 'catalog/filterProductsBySize',
-    //   resetResultProducts: 'catalog/resetResultProducts'
-    // }),
-
-    // filteredProducts() { 
-    //   if(!this.currentFilters.length) return;
-
-    //   this.resetResultProducts()
-      
-    //   this.currentFilters.forEach(item => {
-    //     if (item.type === this.filterTypes.orderby) {
-    //       this.sortProductsByPrice(item.key)
-    //     } else {
-    //       this.filterProductsByAttr(item.type, item.text);
-    //     }
-    //   }); 
-    // },
-
-    // sortProductsByPrice(key) {
-    //   if (key === sortTypes.priceAsc) {
-    //     this.sortProductsByUpPrice()
-    //   } 
-    //   if (key === sortTypes.priceDesc) {
-    //     this.sortProductsByDownPrice()
-    //   }
-    // },
-
-    // filterProductsByAttr(filterType, value) {
-    //   console.log(filterType, value);
-    //   if(filterType === this.filterTypes.brand) {
-    //     this.filterProductsByBrand(value);
-    //   } 
-    //   if (filterType === this.filterTypes.size) {
-    //     this.filterProductsBySize(value);
-    //   }
-    // },
-
-    // filterCollection(selectedOption) {
-    //   const hasFilter = this.currentFilters.some(item => item.type === selectedOption.type);
-
-    //   if (!hasFilter) {
-    //     this.currentFilters.push(selectedOption)
-    //   } else {
-    //     const findedElIndex = this.currentFilters.findIndex(item => item.type === selectedOption.type)
-    //     this.currentFilters.splice(findedElIndex, 1, selectedOption);
-    //   }
-    // },
 
     removeQueryParams(param) {
       const query = { ...this.$route.query };
@@ -128,7 +88,6 @@ export default {
         slug: this.categorySlugFromRoute,
         offset: null
       }).then(result => {
-        // this.countProducts = result?.products_count;
         console.log(result);
       })
       
